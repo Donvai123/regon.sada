82e3ad87a245be44a6f357b4e98591d0b4094ba0
Donvai Regon <sadanandaregon72@gmail.com>
2026-08-21 22:01:59 -0400

    Integrate Firebase authentication and logout functionality

--- afterlogin/script.js
+++ afterlogin/script.js
@@ -1,14 +1,93 @@
-const menuBtn = document.getElementById("menuBtn");
-const navMenu = document.getElementById("navMenu");
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-menuBtn.addEventListener("click", function () {
-    navMenu.classList.toggle("active");
+import {
+    getAuth,
+    onAuthStateChanged,
+    signOut
+}
+from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAj-LOc7fbr1xFs_iNxFwyULwrBUKI-r4k",
+
+    authDomain: "donvai-281a0.firebaseapp.com",
+
+    projectId: "donvai-281a0",
+
+    storageBucket: "donvai-281a0.firebasestorage.app",
+
+    messagingSenderId: "19064202332",
+
+    appId: "1:19064202332:web:e97db30e47c20a6c6cb299"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+
+// =================================
+// CHECK USER LOGIN
+// =================================
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+
+        // User login nahi hai
+        window.location.href = "../login/";
+
+    }
+
 });
 
-const navLinks = document.querySelectorAll("#navMenu a");
 
-navLinks.forEach(function (link) {
-    link.addEventListener("click", function () {
-        navMenu.classList.remove("active");
-    });
+// =================================
+// LOGOUT
+// =================================
+
+const logoutBtn =
+    document.getElementById("logoutBtn");
+
+
+logoutBtn.addEventListener("click", async () => {
+
+    try {
+
+        await signOut(auth);
+
+        // Logout ke baad login page
+        window.location.href = "../login/";
+
+    }
+
+    catch(error) {
+
+        console.log("Logout error:", error);
+
+    }
+
+});
+
+
+// =================================
+// MOBILE MENU
+// =================================
+
+const menuBtn =
+    document.getElementById("menuBtn");
+
+const navMenu =
+    document.getElementById("navMenu");
+
+
+menuBtn.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
+
 });
